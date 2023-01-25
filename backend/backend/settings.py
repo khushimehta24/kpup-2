bf16bf2da1a8c62ca13e2346811271a190ffbf71
Prathambhoir55 <80087152+Prathambhoir55@users.noreply.github.com>
2023-01-25 22:23:19 +0530

    secret key

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,12 +9,8 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import environ
 import os
 from pathlib import Path
-
-env = environ.Env()
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-e5o956+rcpsat$at_$hao47q90043)0-otzjmc!4m)qk56^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
