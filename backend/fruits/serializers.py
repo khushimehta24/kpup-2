dc156562fd02436df02c4de67150e28935e7a2d1
Prathambhoir55 <80087152+Prathambhoir55@users.noreply.github.com>
2023-01-29 01:33:30 +0530

    gitignore changes

--- backend/fruits/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-
-class ImageSerializer(serializers.Serializer):
-    link = serializers.URLField()
-    
-    class Meta:
-        fields = ['link']
--- backend/fruits/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', ImageDetectAPI.as_view(), name = 'image_detect'),
-]
