ee51ec71010af5e1e520ec0a9f212c2550c116e4
Khushi Mehta <khushimehta8282@gmail.com>
2023-01-28 15:22:33 +0530

    barcode integration

--- src/sections/@dashboard/barcode/UploadImg.js
+++ src/sections/@dashboard/barcode/UploadImg.js
@@ -1,6 +1,6 @@
 import { Box } from '@mui/material'
 import React from 'react'
-// import fs from 'fs'
+import fs from 'fs'
 import ImageUploader from 'react-image-upload'
 import 'react-image-upload/dist/index.css'
 import BarcodeService from '../../../services/BarcodeService';
@@ -11,13 +11,13 @@ function UploadImg() {
         const form2Data = new FormData()
         form2Data.append('image', img.file);
         console.log(img.file)
-        // await BarcodeService.getBarcode(fs.createReadStream(img.file))
-        //     .then((res) => {
-        //         console.log(res);
-        //     })
-        //     .catch((e) => {
-        //         console.log(e)
-        //     })
+        await BarcodeService.getBarcode(fs.createReadStream(img.file))
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((e) => {
+                console.log(e)
+            })
         // const res = await IpfsServices.uploadImg(form2Data, token)
         // setJson({ ...json, bannerUri: res.data.urls[0] })
         // console.log(res.data.urls[0])
