a0a7a0d4804178ce2db18ae5079f2e615a90f2eb
Prathambhoir55 <80087152+Prathambhoir55@users.noreply.github.com>
2023-01-29 14:13:52 +0530

    getsetgo url

--- backend/finance/urls.py
+++ backend/finance/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('graph/', GetSetGoAPI.as_view(), name = 'graph'),
+    path('getsetgo/', GetSetGoAPI.as_view(), name = 'getsetgo'),
     
 ]
